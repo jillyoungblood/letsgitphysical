ce73daf0f0e4ebc675a0f224b3ae724e9cd523ac
Jillian Youngblood <jillian.youngblood@gmail.com>
2013-03-07 17:27:35 -0500

    with chart

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,9 @@
 //= require jquery
 //= require jquery_ujs
+//= require 'morris/spec/vendor/raphael-2.1.0.min.js'
+//= require 'morris/morris.min.js'
 //= require 'foundation/js/vendor/custom.modernizr.js'
 //= require 'foundation/js/foundation.min.js'
 //= require 'underscore-min.js'
+//= require 'letsgitphysical.js'
+
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-  def intellinav
+  def login_nav
     if @auth.present?
-      link_to(@auth.email, login_path, :method => :delete)
+      link_to(@auth.email, login_path, :method => :delete, :class => 'button alert')
     else
-      link_to('Login', login_path)
+      link_to('Login', login_path, :class => 'button success')
     end
   end
 end
